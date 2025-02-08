c4892af6a1b3c7b1ec2f2d4dbc52548ae83f343d
tsukasarenato <tsukasa.renato@gmail.com>
2025-02-08 20:19:34 -0300

    Heroku configs

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nome_do_projeto.wsgi --log-file -
--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
